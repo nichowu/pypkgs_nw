223e52d58b2c943bc5648f5c7a7601eb00c9b676
Nicholas Wu <misternicholaswu@gmail.com>
2021-03-11 12:21:14 -0800

    removed testing version assert

--- tests/test_pypkgs_nw.py
+++ tests/test_pypkgs_nw.py
@@ -2,12 +2,21 @@ from pypkgs_nw import __version__
 from pypkgs_nw import pypkgs_nw
 import pandas as pd
 
-def test_version():
-    assert __version__ == '0.1.0'
 
 def test_catbind():
     a = pd.Categorical(["character", "hits", "your", "eyeballs"])
     b = pd.Categorical(["but", "integer", "where it", "counts"])
     assert ((pypkgs_nw.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
-    assert ((pypkgs_nw.catbind(a, b)).categories == ["but", "character",
-            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
+    assert (
+        (pypkgs_nw.catbind(a, b)).categories
+        == [
+            "but",
+            "character",
+            "counts",
+            "eyeballs",
+            "hits",
+            "integer",
+            "where it",
+            "your",
+        ]
+    ).all()
